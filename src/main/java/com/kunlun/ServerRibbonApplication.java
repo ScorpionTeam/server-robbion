6bee649ac92858f85424ded2c4b478f75457d3e4
Jarven <769558579@qq.com>
2017-12-14 17:28:38 +0800

    丢弃Base工程

--- src/main/java/com/kunlun/ServerRibbonApplication.java
+++ src/main/java/com/kunlun/ServerRibbonApplication.java
@@ -5,13 +5,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringCloudApplication
-//@EnableDiscoveryClient
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 public class ServerRibbonApplication {
