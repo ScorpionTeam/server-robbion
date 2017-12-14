e8651cb1b44ad8f52f05a89fceed7c25c63eeb9e
FK <3198141550@qq.com>
2017-12-14 16:57:47 +0800

    微信退款

--- src/main/java/com/kunlun/order/service/WxOrderServiceImpl.java
+++ src/main/java/com/kunlun/order/service/WxOrderServiceImpl.java
@@ -1,6 +1,9 @@
 package com.kunlun.order.service;
 
+import com.github.pagehelper.util.StringUtil;
 import com.kunlun.config.Constants;
+import com.kunlun.entity.Order;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.ModelMap;
@@ -25,9 +28,32 @@ public class WxOrderServiceImpl implements WxOrderService {
      * @param orderStatus
      * @return
      */
+    @HystrixCommand(fallbackMethod = "fallback")
     @Override
     public ModelMap findByUserId(int pageNo, int pageSize, String wxCode, String orderStatus) {
         String url = Constants.SERVER_NAME + Constants.WX_MODULE + "findByUserId/" + pageNo + "/" + pageSize + "/" + wxCode + "/" + orderStatus;
         return restTemplate.getForObject(url, ModelMap.class);
+    }
+
+    /**
+     * 退款
+     *
+     * @param order
+     * @return
+     */
+    @HystrixCommand(fallbackMethod = "fallback")
+    @Override
+    public ModelMap refund(Order order) {
+        ModelMap modelMap = new ModelMap();
+        Long id = order.getId();
+        if (StringUtil.isEmpty(id.toString())) {
+            return modelMap.addAttribute("0003", "查无结果");
+        }
+        String url = Constants.SERVER_NAME + Constants.WX_MODULE + "refund";
+        return restTemplate.postForObject(url, order, ModelMap.class);
+    }
+
+    public ModelMap fallback() {
+        return new ModelMap("ERROR","服务器开小差.....请稍后再试");
     }
 }
