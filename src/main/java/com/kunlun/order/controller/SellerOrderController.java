02988951e2d7fb8b8b7d5e37c99c33f4355af75b
Jarven <769558579@qq.com>
2017-12-14 13:54:32 +0800

    商家订单模块示例

--- src/main/java/com/kunlun/order/controller/SellerOrderController.java
+++ src/main/java/com/kunlun/order/controller/SellerOrderController.java
@@ -1,5 +1,10 @@
 package com.kunlun.order.controller;
 
+import com.kunlun.order.service.SellerOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/seller/order")
 public class SellerOrderController {
+
+    @Autowired
+    private SellerOrderService sellerOrderService;
+
+    @GetMapping("/findByCondition/{pageNo}/{pageSize}")
+    public ModelMap findByCondition(@PathVariable Integer pageNo, @PathVariable Integer pageSize) {
+        return sellerOrderService.findByCondition(pageNo, pageSize);
+    }
+
+
 }
